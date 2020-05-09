9b2e2709ad50ded8f2f884ee54cc00df07a4d0c3
mijkolsmith <mijkolsmith@live.com>
2020-05-09 13:34:41 +0200

    Added a player with walk and jump abilities

---
+++ Assets/Scripts/MyCharacterController.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+
+public class MyCharacterController : MonoBehaviour
+{
+    public CharacterController player;
+
+	[SerializeField]
+    float speed = 6.0f;
+	[SerializeField]
+    float gravity = -20f;
+
+    public Transform groundCheck;
+    public float groundDistance = .4f;
+    public LayerMask groundMask;
+    private bool isGrounded;
+
+    public float jumpHeight = 2.0f;
+    
+    Vector3 velocity;
+    Vector3 move = Vector3.zero;
+
+    void Start()
+    {
+        //hide the mouse
+        Cursor.lockState = CursorLockMode.Locked;
+        player = GetComponent<CharacterController>();
+        groundCheck = transform.GetChild(1).GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        //ground check
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        //reset gravity if grounded
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        if (Input.GetButton("Fire2") == false)
+        {
+            //movement
+            move = transform.right * x + transform.forward * z;
+
+            player.Move(move * speed * Time.deltaTime);
+
+            //gravity
+            if (Input.GetButtonDown("Jump") && isGrounded)
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravity);
+            }
+
+            velocity.y += gravity * Time.deltaTime;
+
+            player.Move(velocity * Time.deltaTime);
+        }
+
+        //toggle the mouse hide with escape
+        if (Input.GetKeyDown("escape"))
+        {
+            if (Cursor.lockState == CursorLockMode.None)
+                Cursor.lockState = CursorLockMode.Locked;
+            else if (Cursor.lockState == CursorLockMode.Locked)
+                Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    public void CheckIsGrounded()
+    {
+        RaycastHit2D[] hits;
+
+        //We raycast down 1 pixel from this position to check for a collider
+        Vector2 positionToCheck = transform.position;
+        hits = Physics2D.RaycastAll(positionToCheck, new Vector2(0, -1), 0.01f);
+
+        //if a collider was hit, we are grounded
+        if (hits.Length > 0)
+        {
+            isGrounded = true;
+        }
+    }
+}
