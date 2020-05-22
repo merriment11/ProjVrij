215b95c74cfa72bf3cbb190e71b3f7ef5e70d02c
mijkolsmith <mijkolsmith@live.com>
2020-05-22 16:49:02 +0200

    disable and enable dof (blur) with 8 and 9 with the help of one simple trick

---
+++ Assets/Scripts/DisableDOF.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class DisableDOF : MonoBehaviour
+{
+	//This is a testing script on how to enable and disable the depth of view.
+	[SerializeField]
+	private PostProcessVolume PostProcessVolume;
+	DepthOfField depthOfField;
+
+    void Update()
+    {
+		if (PostProcessVolume.profile.TryGetSettings(out depthOfField))
+		{
+			if (Input.GetKeyDown(KeyCode.Alpha8))
+			{
+				depthOfField.active = true;
+				Debug.Log("enabled");
+			}
+			if (Input.GetKeyDown(KeyCode.Alpha9))
+			{
+				depthOfField.active = false;
+				Debug.Log("disabled");
+			}
+		}
+	}
+}
