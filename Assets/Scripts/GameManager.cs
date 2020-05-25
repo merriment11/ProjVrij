35eac362e48d077a37416df15b4705883f2fab6f
Dylan Smit <dylan.smit@hotmail.com>
2020-05-25 19:15:17 +0200

    Added keys and key coding for puzzel 3 | Added lighting group for visibility

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int collectedKeys;
+    public bool NewKey;
+    // Start is called before the first frame update
+    void Start()
+    {
+        collectedKeys = 0;
+        NewKey = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (collectedKeys == 1 && NewKey)
+        {
+            Debug.Log(collectedKeys);
+            NewKey = false;
+        }
+        if (collectedKeys == 2 && NewKey)
+        {
+            Debug.Log(collectedKeys);
+            NewKey = false;
+        }
+        if (collectedKeys == 3 && NewKey)
+        {
+            Debug.Log(collectedKeys);
+            UnlockArea();
+            NewKey = false;
+        }
+    }
+
+    private void UnlockArea()
+    {
+        Debug.Log("You did it");
+    }
+}
---
+++ Assets/Scripts/KeyScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyScript : MonoBehaviour
+{
+    GameManager gm;
+
+
+    private void Start()
+    {
+        gm = GameObject.Find("Player").GetComponent<GameManager>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.LogError(collision);
+        gm.collectedKeys++;
+        gm.NewKey = true;
+        this.gameObject.SetActive(false);
+    }
+}
