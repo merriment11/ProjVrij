e990aa7125fd75c194db6db69610fd906e95a221
Dylan Smit <dylan.smit@hotmail.com>
2020-06-18 01:33:52 +0200

    fixed cursor

--- Assets/Scripts/Managers/SceneManager.cs
+++ Assets/Scripts/Managers/SceneManager.cs
@@ -1,7 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneManager : MonoBehaviour
 {
+    private void Start()
+    {
+        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "Credits" || UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "Menu")
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
     public void StartButton()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("MichaelTest");
