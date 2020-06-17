07855c1e3194b06a4acf5f98dd7def02bd2a7b6f
mijkolsmith <mijkolsmith@live.com>
2020-06-17 14:15:47 +0200

    Massive update for this one single script

--- Assets/Scripts/SnapScript.cs
+++ Assets/Scripts/SnapScript.cs
@@ -6,20 +6,18 @@ public class SnapScript : MonoBehaviour
 {
 	public int vision = 1;
 	private float growSpeed = 0.2f;
-	private float shrinkSpeed = 0.04f;
 	private float minSize = 3.5f;
-	private float maxSize = 18;
-	private float pauseShrink = 0.5f;
 
 	public Image fade;
-	public IEnumerator coroutine;
 
 	public float size; //for the raycast script
 
 	GameObject player;
 	public GameObject spherePrefab;
 
-	public AudioSource Right;	
+	public AudioSource Right;
+
+	UnityEngine.SceneManagement.Scene scene;
 
 	private void Start()
 	{
@@ -27,7 +25,7 @@ public class SnapScript : MonoBehaviour
 		transform.localScale = new Vector3(minSize, minSize, minSize);
 
 		player = transform.parent.parent.gameObject;
-		coroutine = GrowCircle();
+		scene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
 	}
 
 	private void Update()
@@ -40,35 +38,20 @@ public class SnapScript : MonoBehaviour
 		switch (vision) //levels of vision
 		{
 			case (0): //only for menu
-				growSpeed = 200;
-				shrinkSpeed = 125;
+				growSpeed = 250;
 				minSize = 3.5f;
-				maxSize = 500;
-				pauseShrink = 0.5f;
 				break;
 			case (1):
-				growSpeed = 200;
-				shrinkSpeed = 125;
+				growSpeed = 250;
 				minSize = 3.5f;
-				maxSize = 13;
-				pauseShrink = 0.5f;
 				break;
 			case (2):
-				growSpeed = 225;
-				shrinkSpeed = 125;
+				growSpeed = 250;
 				minSize = 3.5f;
-				maxSize = 15;
-				pauseShrink = 0.5f;
 				break;
 			case (3):
 				growSpeed = 250;
-				shrinkSpeed = 125;
 				minSize = 3.5f;
-				maxSize = 18;
-				pauseShrink = 0.5f;
-				break;
-			case (4): //this triggers if you press too early
-				shrinkSpeed = 400;
 				break;
 		}
 
@@ -79,14 +62,21 @@ public class SnapScript : MonoBehaviour
 		{
 			if (xSpeed <= .25f && -.25f <= xSpeed && zSpeed <= .25f && -.25f <= zSpeed)
 			{
-				StartCoroutine(coroutine); //start the growth of the sphere so you can see
+				if (scene.name == "Menu")
+				{
+					StartCoroutine(GrowCircleMenu()); //start the growth of the sphere so you can see
+				}
+				else
+				{
+					StartCoroutine(GrowCircle());
+				}
 			}
 		}
 
 		size = transform.localScale.x; //so the raycast script can access it
 	}
 
-	/*IEnumerator GrowCircleNew()
+	IEnumerator GrowCircle()
 	{
 		float i = 0; //the for loops didn't work well, so we decided to use while loops. The 'I' is kept from this
 		Right.Play();
@@ -100,60 +90,28 @@ public class SnapScript : MonoBehaviour
 			if (Input.GetButtonDown("Snap") && transform.localScale.x > minSize + 1)
 			{
 				transform.localScale = new Vector3(minSize, minSize, minSize); //reset the sphere
-				fade.color = Color.clear;
-				StopCoroutine(coroutine);
+				fade.color = Color.clear;	
+				yield break;
 			}
 		}
 			
 		transform.localScale = new Vector3(minSize, minSize, minSize); //reset the sphere
 		fade.color = Color.clear;
 		yield return null;
-	}*/
+	}
 
-	IEnumerator GrowCircle() 
+	IEnumerator GrowCircleMenu() 
 	{
-		int tempvision = vision;
-		bool broken = false;
 		float i = 0; //the for loops didn't work well, so we decided to use while loops. The 'I' is kept from this
 		Right.Play();
-		while (i / 25 <= maxSize - minSize) //grow the sphere
+		while (i / 25 <= 500) //grow the sphere
 		{
 			i += Time.deltaTime * growSpeed;
-			yield return null;
-			transform.localScale = new Vector3(minSize + i / 25, minSize + i / 25, minSize + i / 25);
-			if (Input.GetButtonDown("Snap") && transform.localScale.x > minSize + 1)
-			{
-				vision = 4;
-				broken = true;
-				break;
-			}
-		}
-
-		if (broken == false)
-		{
-			yield return new WaitForSeconds(pauseShrink); //a little extra time to see
-		}
-
-
-		while (i >= minSize) //shrink the sphere
-		{
-			if (Input.GetButtonDown("Snap") && transform.localScale.x > minSize + 1)
-			{
-				broken = true;
-				vision = 4;
-			}
-			i -= Time.deltaTime * shrinkSpeed;
 			yield return null;
 			transform.localScale = new Vector3(minSize + i / 25, minSize + i / 25, minSize + i / 25);
 		}
 
 		transform.localScale = new Vector3(minSize, minSize, minSize); //reset the sphere
-		vision = tempvision;
-
-		if (broken == true)
-		{
-			StartCoroutine("GrowCircle");
-		}
 
 		yield return null;
 	}
